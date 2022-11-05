635a691aa895a5a2268dfaa458835f5cae49499b
Juan Pablo Sanchez <jp.sanchez@uc.cl>
2022-11-05 01:21:00 -0300

    test function

--- src/server/preparation/preparation.c
+++ src/server/preparation/preparation.c
@@ -18,7 +18,7 @@ int count_placed_ships(char** board) {
     int placed_ships = 0;
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
-            if (board[i, j] == 'O') {
+            if (board[i][j] == 'O') {
                 o_count++;
             }
         }
