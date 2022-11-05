e5668e9fa3beacb1bae17424a0999505b312d9d9
Juan Pablo Sanchez <jp.sanchez@uc.cl>
2022-11-05 03:21:25 -0300

    test function

--- src/server/preparation/preparation.c
+++ src/server/preparation/preparation.c
@@ -87,7 +87,9 @@ void place_ship(char** board, char* start, char* end) {
             board[start_pos[0]][i] = 'O';
         }
     } else if (start_pos[1] == end_pos[1]) {
-        for (int i = start_pos[0]; i <= end_pos[0]; i++) {
+        int range[] = {start_pos[0], end_pos[0]};
+        sort_range(range);
+        for (int i = range[0]; i <= range[1]; i++) {
             board[i][start_pos[1]] = 'O';
         }
     }
