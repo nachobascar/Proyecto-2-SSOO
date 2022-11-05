04000d013ec9778c98f0d85948a9d1c73bc00ac8
Juan Pablo Sanchez <jp.sanchez@uc.cl>
2022-11-05 03:55:32 -0300

    test function

--- src/server/preparation/preparation.c
+++ src/server/preparation/preparation.c
@@ -73,6 +73,26 @@ void sort_range(int* range) {
     }
 }
 
+int check_pos(char* start, char* end, int ship_length, int placed_ships) {
+    int condition_1 = 0;
+    int condition_2 = 0;
+
+    if (start[0] >= 0 && start[0] <= 4 && start[1] >= 0 && start[1] <= 4
+        && end[0] >= 0 && end[0] <= 4 && end[1] >= 0 && end[1] <= 4) {
+        condition_1 = 1;
+    }
+    if ((ship_length == 2 && placed_ships == 0)
+        || (ship_length == 3 && placed_ships == 1)
+        || (ship_length == 4 && placed_ships == 2)) {
+        condition_2 = 1;
+    }
+    
+    if (condition_1 && condition_2) {
+        return 1;
+    }
+    return 0;
+}
+
 void place_ship(char** board, char* start, char* end) {
     int start_pos[] = {(int) start[1] - '0' - 1, char_to_pos(start[0])};
     int end_pos[] = {(int) end[1] - '0' - 1, char_to_pos(end[0])};
@@ -89,6 +109,11 @@ void place_ship(char** board, char* start, char* end) {
                 return;
             }
         }
+        int ship_length = range[1] - range[0] + 1;
+        if (!check_pos(start, end, ship_length, count_placed_ships(board))) {
+            printf("F\n");
+            return;
+        }
         for (int i = range[0]; i <= range[1]; i++) {
             board[start_pos[0]][i] = 'O';
         }
@@ -102,9 +127,16 @@ void place_ship(char** board, char* start, char* end) {
                 return;
             }
         }
+        int ship_length = range[1] - range[0] + 1;
+        if (!check_pos(start, end, ship_length, count_placed_ships(board))) {
+            printf("F\n");
+            return;
+        }
         for (int i = range[0]; i <= range[1]; i++) {
             board[i][start_pos[1]] = 'O';
         }
+    } else {
+        printf("F\n");
     }
 }
 
