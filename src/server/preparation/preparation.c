7865bc68bd2a71e6781affa1cd2ed8c0c520b448
Juan Pablo Sanchez <jp.sanchez@uc.cl>
2022-11-05 00:42:35 -0300

    test function

--- src/server/preparation/preparation.c
+++ src/server/preparation/preparation.c
@@ -7,21 +7,6 @@ void close_board(char** board) {
     free(board);
 }
 
-void test_function() {
-    char** grid = malloc(5 * sizeof(char*));
-    for (int i = 0; i < 5; i++) {
-        grid[i] = malloc(5 * sizeof(char));
-        for (int j = 0; j < 5; j++) {
-            char c = 'O';
-            grid[i][j] = c;
-        }
-    }
-
-    print_grid(grid);
-
-    close_grid(grid);
-}
-
 void create_player_board(player player) {
     char** grid = malloc(5 * sizeof(char*));
     for (int i = 0; i < 5; i++) {
