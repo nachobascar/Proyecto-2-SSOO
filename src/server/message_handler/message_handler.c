c16bf176b83e03dcd0beddf6cc541f917bb5f9a7
Juan Pablo Sanchez <jp.sanchez@uc.cl>
2022-11-07 21:38:18 -0300

    Added ship length to board

--- src/server/message_handler/message_handler.c
+++ src/server/message_handler/message_handler.c
@@ -101,3 +101,23 @@ void handle_id_2(player* player, server *server, int id, int data_length, char *
   }
   send_package(player->socket, 0, data_length, data, server);
 }
+
+// Request for updated rooms list
+void handle_id_3(player* player, server *server, int id, int data_length, char *data) {
+  // Send to the player the list of rooms
+  char data[255];
+  int data_length = 1;
+  data[0] = 1;
+  for (int i = 0; i < server->rooms_size; i++) {
+    char room_id[4];
+    sprintf(room_id, "%d", i);
+    char n_players[4];
+    sprintf(n_players, "%d", server->rooms[i].n_players);
+    
+    strcpy(data + data_length, room_id);
+    data_length += 4;
+    strcpy(data + data_length, n_players);
+    data_length += 4;   
+  }
+  send_package(player->socket, 0, data_length, data, server);
+}
