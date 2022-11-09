06a00225951b3e1d60224084780f15f4ee597749
Juan Pablo Sanchez <jp.sanchez@uc.cl>
2022-11-09 01:47:22 -0300

    Preparation Phase

--- src/server/message_handler/message_handler.c
+++ src/server/message_handler/message_handler.c
@@ -229,4 +229,18 @@ void handle_id_6(player* player, server* server, int id, int data_length, char* 
 	char end[] = {data[2], data[3]};
 	printf("Start: %s\n", start);
 	printf("End: %s\n", start);
+
+	if (count_placed_ships(player->board) != 3) {
+		int status = place_ship(player->board, start, end);
+		if (status == 0) {
+			print_grid(player->board);
+		} else {
+			char* error_msg = "Las coordenadas ingresadas son inválidas. Recuerda lo siguiente\n"
+			"\t- Seguir el formato para ingresar coordenadas\n"
+			"\t- Los barcos solo pueden estar horizontal o verticalmente\n"
+			"\t- Primero debes ingresar un barco de largo 2, luego 3 y luego 4\n"
+			"\t- Un barco no puede chocar con otro\n\n";
+		  printf("%s", error_msg);
+		}
+  }
 }
--- src/server/preparation/preparation.h
+++ src/server/preparation/preparation.h
@@ -15,3 +15,10 @@ void start_preparation(char** board);
 
 // Free board memory
 void close_board(char** board);
+
+// Return the number of placed ships in a board
+int count_placed_ships(char** board);
+
+// Place a ship in the board given start and end position
+// Return 0 if placed correctly, if not return -1
+int place_ship(char** board, char* start, char* end);
